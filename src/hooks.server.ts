b6da5e15660323cf8fa620946097daa3d6839c19
a6iyyu <mizukinako7@gmail.com>
2026-08-18 12:28:05 +0700

    chore: Add dependencies and improve CI/CD on deploy.yml

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,11 @@ import type { Handle } from "@sveltejs/kit";
 import { getTextDirection } from "$lib/paraglide/runtime";
 import { paraglideMiddleware } from "$lib/paraglide/server";
 
-const paraglide: Handle = ({ event, resolve }) => paraglideMiddleware(event.request, ({ request, locale }) => {
+const paraglide: Handle = ({ event, resolve }) => paraglideMiddleware(event.request, ({ request, locale }: { request: Request; locale: string }) => {
 	event.request = request;
 
 	return resolve(event, {
-		transformPageChunk: ({ html }) => html.replace("%paraglide.lang%", locale).replace("%paraglide.dir%", getTextDirection(locale)),
+		transformPageChunk: ({ html }) => html.replace("%lang%", locale).replace("%dir%", getTextDirection(locale))
 	});
 });
 
